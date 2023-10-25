953f0aab48b7f94f0340bb83656041fad3a5d774
teenspirittt <vshabunin25@gmail.com>
2023-10-26 00:23:33 +0700

    update route gateway

--- src/gateways/RouteGateway.cpp
+++ src/gateways/RouteGateway.cpp
@@ -28,3 +28,170 @@ bool RouteGateway::deleteRoute(int routeId) {
   std::string query = "DELETE FROM Route WHERE id = " + std::to_string(routeId);
   return sqlExecuter->executeSQL(query);
 }
+
+bool RouteGateway::updateRoute(int routeId, const std::string &name,
+                               const std::string &country,
+                               const std::string &city, int duration,
+                               int hotelId, int flightId, int employeeId,
+                               const std::string &agencyRepName,
+                               const std::string &agencyRepPhone) {
+  std::string query = "UPDATE Route SET name = '" + name + "', country = '" +
+                      country + "', city = '" + city +
+                      "', duration = " + std::to_string(duration) +
+                      ", hotel_id = " + std::to_string(hotelId) +
+                      ", flight_id = " + std::to_string(flightId) +
+                      ", employee_id = " + std::to_string(employeeId) +
+                      ", agency_representative_name = '" + agencyRepName +
+                      "', agency_representative_phone = '" + agencyRepPhone +
+                      "' WHERE id = " + std::to_string(routeId);
+
+  return sqlExecuter->executeSQL(query);
+}
+
+bool RouteGateway::getAllRoutes(std::vector<int> &routeIds) {
+  std::string query = "SELECT id FROM Route";
+  SQLHSTMT hstmt;
+
+  if (sqlExecuter->executeSQLWithResults(query, hstmt)) {
+    SQLRETURN ret = SQL_SUCCESS;
+    while (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+      int routeId;
+      ret = SQLGetData(hstmt, 1, SQL_C_LONG, &routeId, 0, NULL);
+      if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+        routeIds.push_back(routeId);
+      }
+    }
+
+    SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+    return !routeIds.empty();
+  }
+  std::cerr << "Failed to execute SQL or no routes found." << std::endl;
+  return false;
+}
+
+bool RouteGateway::getRoute(int routeId, std::string &name,
+                            std::string &country, std::string &city,
+                            int &duration, int &hotelId, int &flightId,
+                            int &employeeId, std::string &agencyRepName,
+                            std::string &agencyRepPhone) {
+  std::string query =
+      "SELECT name, country, city, duration, hotel_id, flight_id, employee_id, "
+      "agency_representative_name, agency_representative_phone "
+      "FROM Route WHERE id = " +
+      std::to_string(routeId);
+
+  SQLHSTMT hstmt;
+  char nameBuffer[256];
+  char countryBuffer[256];
+  char cityBuffer[256];
+  char agencyRepNameBuffer[256];
+  char agencyRepPhoneBuffer[256];
+
+  if (sqlExecuter->executeSQLWithResults(query, hstmt)) {
+    SQLRETURN ret = SQL_SUCCESS;
+    ret = SQLFetch(hstmt);
+    if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+      SQLLEN nameLength, countryLength, cityLength, agencyRepNameLength,
+          agencyRepPhoneLength;
+      ret = SQLGetData(hstmt, 1, SQL_C_CHAR, nameBuffer, sizeof(nameBuffer),
+                       &nameLength);
+      ret = SQLGetData(hstmt, 2, SQL_C_CHAR, countryBuffer,
+                       sizeof(countryBuffer), &countryLength);
+      ret = SQLGetData(hstmt, 3, SQL_C_CHAR, cityBuffer, sizeof(cityBuffer),
+                       &cityLength);
+      ret = SQLGetData(hstmt, 4, SQL_C_LONG, &duration, 0, NULL);
+      ret = SQLGetData(hstmt, 5, SQL_C_LONG, &hotelId, 0, NULL);
+      ret = SQLGetData(hstmt, 6, SQL_C_LONG, &flightId, 0, NULL);
+      ret = SQLGetData(hstmt, 7, SQL_C_LONG, &employeeId, 0, NULL);
+      ret = SQLGetData(hstmt, 8, SQL_C_CHAR, agencyRepNameBuffer,
+                       sizeof(agencyRepNameBuffer), &agencyRepNameLength);
+      ret = SQLGetData(hstmt, 9, SQL_C_CHAR, agencyRepPhoneBuffer,
+                       sizeof(agencyRepPhoneBuffer), &agencyRepPhoneLength);
+
+      name = nameBuffer;
+      country = countryBuffer;
+      city = cityBuffer;
+      agencyRepName = agencyRepNameBuffer;
+      agencyRepPhone = agencyRepPhoneBuffer;
+
+      SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+      return true;
+    } else {
+      SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+    }
+  }
+
+  std::cerr << "Failed to execute SQL or no route found." << std::endl;
+  return false;
+}
+
+bool RouteGateway::findRoutesByHotel(int hotelId,
+                                     std::vector<int> &matchingRouteIds) {
+  std::string query =
+      "SELECT id FROM Route WHERE hotel_id = " + std::to_string(hotelId);
+  SQLHSTMT hstmt;
+
+  if (sqlExecuter->executeSQLWithResults(query, hstmt)) {
+    SQLRETURN ret = SQL_SUCCESS;
+    while (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+      int routeId;
+      ret = SQLGetData(hstmt, 1, SQL_C_LONG, &routeId, 0, NULL);
+      if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+        matchingRouteIds.push_back(routeId);
+      }
+    }
+
+    SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+    return !matchingRouteIds.empty();
+  }
+  std::cerr << "Failed to execute SQL or no matching routes found."
+            << std::endl;
+  return false;
+}
+
+bool RouteGateway::findRoutesByEmployee(int employeeId,
+                                        std::vector<int> &matchingRouteIds) {
+  std::string query =
+      "SELECT id FROM Route WHERE employee_id = " + std::to_string(employeeId);
+  SQLHSTMT hstmt;
+
+  if (sqlExecuter->executeSQLWithResults(query, hstmt)) {
+    SQLRETURN ret = SQL_SUCCESS;
+    while (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+      int routeId;
+      ret = SQLGetData(hstmt, 1, SQL_C_LONG, &routeId, 0, NULL);
+      if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+        matchingRouteIds.push_back(routeId);
+      }
+    }
+
+    SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+    return !matchingRouteIds.empty();
+  }
+  std::cerr << "Failed to execute SQL or no matching routes found."
+            << std::endl;
+  return false;
+}
+
+bool RouteGateway::findRoutesByCity(const std::string &city,
+                                    std::vector<int> &matchingRouteIds) {
+  std::string query = "SELECT id FROM Route WHERE city = '" + city + "'";
+  SQLHSTMT hstmt;
+
+  if (sqlExecuter->executeSQLWithResults(query, hstmt)) {
+    SQLRETURN ret = SQL_SUCCESS;
+    while (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+      int routeId;
+      ret = SQLGetData(hstmt, 1, SQL_C_LONG, &routeId, 0, NULL);
+      if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+        matchingRouteIds.push_back(routeId);
+      }
+    }
+
+    SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+    return !matchingRouteIds.empty();
+  }
+  std::cerr << "Failed to execute SQL or no matching routes found."
+            << std::endl;
+  return false;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -29,62 +29,6 @@ int main() {
       std::cout << "Init database error";
     }
 
-    EmployeeGateway employeeGateway;
-
-    // Вставляем нового сотрудника
-    if (employeeGateway.insertEmployee("John Doe", "123 Main St", "1990-01-15",
-                                       "Manager", 55000.0)) {
-      std::cout << "Employee inserted successfully." << std::endl;
-    } else {
-      std::cerr << "Failed to insert employee." << std::endl;
-    }
-
-    // Удаление сотрудника по ID (замените 1 на действительный ID)
-    if (employeeGateway.deleteEmployee(3)) {
-      std::cout << "Employee deleted successfully." << std::endl;
-    } else {
-      std::cerr << "Failed to delete employee." << std::endl;
-    }
-
-    std::string fullName, address, dateOfBirth, position;
-    double salary;
-
-    // Получение данных о сотруднике (замените 2 на действительный ID)
-    if (employeeGateway.getEmployee(2, fullName, address, dateOfBirth, position,
-                                    salary)) {
-      std::cout << "Employee get successfully." << std::endl;
-      std::cout << "Full Name: " << fullName << std::endl;
-      std::cout << "Address: " << address << std::endl;
-      std::cout << "Date of Birth: " << dateOfBirth << std::endl;
-      std::cout << "Position: " << position << std::endl;
-      std::cout << "Salary: " << salary << std::endl;
-    } else {
-      std::cerr << "Failed to get employee." << std::endl;
-    }
-
-    // Обновление данных о сотруднике (замените 3 на действительный ID)
-    if (employeeGateway.updateEmployee(13, "Alice Smith", "456 Elm St",
-                                       "1988-06-20", "Supervisor", 60000.0)) {
-      std::cout << "Employee updated successfully." << std::endl;
-    } else {
-      std::cerr << "Failed to update employee." << std::endl;
-    }
-
-    std::vector<int> matchingEmployeeIds;
-
-    // Поиск сотрудников по должности
-    if (employeeGateway.findEmployeesByPosition("Manager",
-                                                matchingEmployeeIds)) {
-      std::cout
-          << "Employees with the position 'Manager' were found successfully."
-          << std::endl;
-      for (int employeeId : matchingEmployeeIds) {
-        std::cout << "Matching Employee ID: " << employeeId << std::endl;
-      }
-    } else {
-      std::cerr << "Failed to find employees by position." << std::endl;
-    }
-
     dbConnector.disconnect();
   } else {
     std::cerr << "Failed to connect to the database." << std::endl;
